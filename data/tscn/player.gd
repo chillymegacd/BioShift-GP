a916590f3da25e824d7fcb48e4f01cf2c32b70c3
chillymegacd <125151540+chillymegacd@users.noreply.github.com>
2025-01-06 18:34:04 -0700

    Day 2

--- data/tscn/player.gd
+++ data/tscn/player.gd
@@ -2,7 +2,7 @@ extends "res://src/character.gd"
 
 func _physics_process(delta: float) -> void:
 	forward_influence = Input.get_action_strength("accel1")
-	speed -= move_speed * Input.get_action_strength("brake1") * delta
+	speed -= move_speed * Input.get_action_strength("brake1") * delta * .25
 	turn_influence = Input.get_axis("turn_r1", "turn_l1") * ( 1 - ((speed / move_speed) * 0.8))
 	update_character(delta)
 	$Camera3D.fov = 75 + ((speed / move_speed) * 10)
